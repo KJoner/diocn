4ea5024619e1878594606cffacf6c06a8bd57e15
KJoner <yiwuxukangjing@gmail.com>
2017-07-11 15:06:05 +0800

    first version

---
+++ src/main/java/com/diocn/App.java
@@ -0,0 +1,20 @@
+package com.diocn;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    @Test
+    public void test01(){
+    	System.out.println("ok");
+    }
+    
+}
